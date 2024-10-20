f4ad34f04d18f1bd82f715bc5e99fe2c23e4a3b2
Ce Sui <suic20@mails.tsinghua.edu.cn>
2024-10-20 18:30:32 +0200

    Add emulators for extended cosmological models (#14)

--- tests/test_syren.py
+++ tests/test_syren.py
@@ -9,8 +9,13 @@ import scipy.special
 import symbolic_pofk.linear as linear
 import symbolic_pofk.syrenhalofit as syrenhalofit
 
+import symbolic_pofk.linear_new as linear_new
+import symbolic_pofk.syren_new as syren_new
+
 import symbolic_pofk.pytorch.linear as torch_linear
 import symbolic_pofk.pytorch.syrenhalofit as torch_syrenhalofit
+import symbolic_pofk.pytorch.linear_new as torch_linear_new
+import symbolic_pofk.pytorch.syren_new as torch_syren_new
 import symbolic_pofk.pytorch.utils as torch_utils
 
 
@@ -156,8 +161,6 @@ def test_lcdm():
     A_check = pk_syrenhalofit / pk_bartlett - 1
     assert np.allclose(A_emu, A_check, atol=1e-4)
 
-    return
-
 
 def test_lcdm_torch():
 
@@ -219,15 +222,16 @@ def test_lcdm_torch():
     # Check syren linear at various z matches numpy version
     for emulator in ['fiducial', 'max_precision']:
         for extrapolate in [True, False]:
-            for z_ in np.linspace(0, 2, 5):
-                a_ = 1 / (1+z_)
-                theta_temp = torch.tensor([sigma8, Om, Ob, h, ns, a_],
-                                          requires_grad=True).reshape(1, -1)
-                pk = linear.plin_emulated(
-                    k, sigma8, Om, Ob, h, ns, a_, emulator=emulator, extrapolate=extrapolate)
-                torch_pk = torch_linear.plin_emulated(
-                    kt, theta_temp, emulator=emulator, extrapolate=extrapolate)
-                assert np.allclose(pk, torch_pk.detach().numpy(), rtol=1e-4)
+            for use_approx_D in [True, False]:
+                for z_ in np.linspace(0, 2, 5):
+                    a_ = 1 / (1+z_)
+                    theta_temp = torch.tensor([sigma8, Om, Ob, h, ns, a_],
+                                            requires_grad=True).reshape(1, -1)
+                    pk = linear.plin_emulated(
+                        k, sigma8, Om, Ob, h, ns, a_, emulator=emulator, extrapolate=extrapolate)
+                    torch_pk = torch_linear.plin_emulated(
+                        kt, theta_temp, emulator=emulator, extrapolate=extrapolate, use_approx_D=use_approx_D)
+                    assert np.allclose(pk, torch_pk.detach().numpy(), rtol=5e-3)
 
     # Check asking for a different emulator raises NotImplementedError
     unittest.TestCase().assertRaises(
@@ -269,8 +273,8 @@ def test_lcdm_torch():
             pk = syrenhalofit.run_halofit(k, sigma8, Om, Ob, h, ns, a_,
                                           emulator=emulator, which_params=which_params, add_correction=add_correction, extrapolate=extrapolate)
             torch_pk = torch_syrenhalofit.run_halofit(
-                kt, theta_temp, emulator=emulator, which_params=which_params, add_correction=add_correction, extrapolate=extrapolate)
-            assert np.allclose(pk, torch_pk.detach().numpy(), rtol=1e-4)
+                kt, theta_temp, emulator=emulator, which_params=which_params, add_correction=add_correction, extrapolate=extrapolate)[0]
+            assert np.allclose(pk, torch_pk.detach().numpy(), rtol=5e-3)
 
     # Check asking for a different halofit raises NotImplementedError
     unittest.TestCase().assertRaises(
@@ -446,5 +450,148 @@ def test_utils_torch():
         max_iter=10000,
         tolerance=1.0e-6
     )
+
+    return
+  
+  
+def test_syren_new():
+
+    # Define k range
+    kmin = 9e-3
+    kmax = 9
+    nk = 400
+    k = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+
+    # Cosmological parameters
+    As = 2.105  # 10^9 A_s
+    h = 0.6766
+    Om = 0.3111
+    Ob = 0.02242 / h ** 2
+    ns = 0.9665
+    tau = 0.0561
+    mnu = 0.10
+    w0 = -0.9
+    wa = 0.1
+
+    # Redshift
+    z = 1
+    a = 1 / (1+z)
+    
+    # Get sigma8 for this As
+    sigma8 = linear_new.As_to_sigma8(As, Om, Ob, h, ns, mnu, w0, wa)
+    sigma8_max_prec = linear_new.As_to_sigma8_max_precision(As, Om, Ob, h, ns, mnu, w0, wa)
+
+    # See what As you get in reverse
+    As_new = linear_new.sigma8_to_As(sigma8, Om, Ob, h, ns, mnu, w0, wa)
+    As_new_max_prec = linear_new.sigma8_to_As_max_precision(sigma8_max_prec, Om, Ob, h, ns, mnu, w0, wa)
+    assert math.isclose(As, As_new, rel_tol=1e-2)
+    assert math.isclose(As, As_new_max_prec, rel_tol=1e-4)
+    assert math.isclose(sigma8, sigma8_max_prec, rel_tol=1e-2)
+
+    # Compute P(k) using camb
+    num_massive_neutrinos = (3
+                             if mnu != 0.0
+                             else 0)
+    Oc = Om - Ob - mnu / 93.14 / h ** 2
+    redshift = 1 / a - 1
+    pars = camb.CAMBparams()
+    pars.set_cosmology(
+        H0=100 * h,
+        ombh2=(Ob * h ** 2),
+        omch2=(Oc * h**2),
+        omk=0,
+        neutrino_hierarchy='degenerate',
+        num_massive_neutrinos=num_massive_neutrinos,
+        mnu=mnu,
+        standard_neutrino_neff=3.046,
+        tau=tau
+    )
+    pars.set_dark_energy(w=w0, wa=wa)
+    pars.InitPower.set_params(ns=ns, As=As*1e-9, r=0)
+    pars.set_matter_power(redshifts=[redshift], kmax=k[-1],
+                          accurate_massive_neutrino_transfers=True)
+    pars.NonLinear = camb.model.NonLinear_none
+    results = camb.get_results(pars)
+    index = 6
+    _, _, plin_camb = results.get_matter_power_spectrum(
+        var1=(1 + index),
+        var2=(1 + index),
+        minkh=k.min(),
+        maxkh=k.max(),
+        npoints=len(k)
+    )
+    plin_camb = plin_camb[0]
+
+    # Get emulated power spectrum
+    plin_syren_new = linear_new.plin_new_emulated(
+        k, As, Om, Ob, h, ns, mnu, w0, wa, a=a)
+
+    # Check that the linear emulator is close to camb
+    assert np.allclose(np.log(plin_camb), np.log(plin_syren_new), atol=1e-2)
+
+    # Get camb halofit
+    pars.NonLinear = camb.model.NonLinear_both
+    pars.NonLinearModel.set_params(halofit_version='takahashi')
+    results = camb.get_results(pars)
+    nk = len(k)
+    kh, z, pk_camb_halofit = results.get_matter_power_spectrum(
+        minkh=k[0], maxkh=k[-1], npoints=nk)
+    pk_camb_halofit = pk_camb_halofit[0]
+
+    # Get syren halofit
+    pk_syren_new = syren_new.pnl_new_emulated(
+        k, As, Om, Ob, h, ns, mnu, w0, wa, a)
+
+    # Check that the emulator is close to camb
+    assert np.allclose(np.log(pk_camb_halofit),
+                       np.log(pk_syren_new), atol=1e-1)
+
+    #  Check that the result at minimum k is close to linear
+    assert np.allclose(np.log(plin_syren_new[0]), np.log(
+        pk_syren_new[0]), atol=1e-2)
+
+    return
+
+
+def test_torch_syren_new():
+
+    # Define k range
+    kmin = 9e-3
+    kmax = 9
+    nk = 400
+    k = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+
+    # Cosmological parameters
+    As = 2.105  # 10^9 A_s
+    h = 0.6766
+    Om = 0.3111
+    Ob = 0.02242 / h ** 2
+    ns = 0.9665
+    mnu = 0.10
+    w0 = -0.9
+    wa = 0.1
+
+    # Redshift
+    z = 1
+    a = 1 / (1+z)
+
+    # Get numpy versions
+    plin_numpy = linear_new.plin_new_emulated(
+        k, As, Om, Ob, h, ns, mnu, w0, wa, a=a)
+    pnl_numpy = syren_new.pnl_new_emulated(
+        k, As, Om, Ob, h, ns, mnu, w0, wa, a)
+
+    # Get torch versions
+    theta = torch.tensor([As, Om, Ob, h, ns, mnu, w0, wa, a],
+                         requires_grad=True).reshape(1, -1)
+    k = torch.tensor(k, requires_grad=True)
+    plin_torch = torch_linear_new.plin_new_emulated(k, theta)
+    pnl_torch = torch_syren_new.pnl_new_emulated(k, theta)
+
+    # Check that the results are close
+    assert np.allclose(np.log(plin_numpy), np.log(
+        plin_torch.detach().numpy()), atol=1e-6)
+    assert np.allclose(np.log(pnl_numpy), np.log(
+        pnl_torch.detach().numpy()), atol=1e-6)
 
     return
